03a9a357b2b57be2d06bf06e0392a8a68e596820
Ignacio Cl <juanigcl2532@gmail.com>
2021-07-23 13:33:38 -0600

    Agrego musicPlayer version completa

--- js/MusicPlayer.js
+++ js/MusicPlayer.js
@@ -1,81 +1,69 @@
-import SongPlayer from "./modules/SongPlayer.js";
-let canvas = document.getElementById("musicPlayerCanvas");
-let ctx = canvas.getContext("2d");
-let width = canvas.width;
-let height = canvas.height;
-let music = document.querySelector('.music');
-let playButton = document.querySelector('.playButton')
+import SongPlayer from './modules/SongPlayer';
 
-function songs(data){
-	music.src = data;
-	music.load();
+const canvas = document.getElementById('musicPlayerCanvas');
+const ctx = canvas.getContext('2d');
+const music = document.querySelector('.music');
+const buttonPlayer = document.querySelectorAll('.js__listening');
+const width = canvas.width;
+const height = canvas.height;
+const playButton = document.querySelector('.playButton');
+let musicController = '';
+
+function songCreation(audio) {
+  const context = new AudioContext(); 
+  const src = context.createMediaElementSource(audio);
+  const analyser = context.createAnalyser(); 
+  analyser.connect(context.destination);  
+  analyser.fftSize = 512; 
+  src.connect(analyser);
+  const dataArray = new Uint8Array(analyser.frequencyBinCount); 
+  const bufferLength = analyser.frequencyBinCount; 
+  const barWidth = (width / bufferLength) * 1.5;
+  let barHeight;
+  let x = 0;
+
+  function songAnimation() {
+    x = 0;
+    analyser.getByteFrequencyData(dataArray);
+    ctx.fillStyle = '#2A2438';
+    ctx.fillRect(0, 0, width, height);
+    for (let i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i] + 150;
+      const r = barHeight / 1.8 + ((i / bufferLength)) - 15;
+      const g = 110 * (i / bufferLength);
+      const b = 120 + i;
+      ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+      ctx.fillRect(x, height - barHeight, barWidth, barHeight);
+      x += barWidth + 1;
+    }
+    requestAnimationFrame(songAnimation);
+  }
+  songAnimation();
 }
 
-function start(song){
-	playButton.addEventListener('click', (e) => {
-		if(e.target.classList.contains('active')){
-			playButton.src = '../img/play-button.png';
-			song.pause();
-		}else{
-			playButton.src = '../img/stop-button.png';
-			song.play();
-		}
-		e.target.classList.toggle('active');
-	})
+function start(song) {
+  playButton.addEventListener('click', (e) => {
+    songCreation(song);
+    if (e.target.classList.contains('active')) {
+      playButton.src = '../img/play-button.png';
+      song.pause();
+    } else {
+      playButton.src = '../img/stop-button.png';
+      song.play();
+    }
+    e.target.classList.toggle('active');
+  });
 }
 
-fetch('https://kt2ul4cwza.execute-api.us-east-2.amazonaws.com/public/songs/aurora', {
-	method: "GET"
-})
-	.then((response) => {
-	if (!response.ok) {
-		throw new Error("Network response was not ok " + response.status);
-	}
-	return response.json();
-	})
-	.then((data) => {
-		const SongsList = new SongPlayer(data);
-		SongsList.previous();
-		SongsList.next();
-		for(let i = 0; i < data.length; i++){
-			let currentSong = data[i].audio;
-			songs(currentSong);
-			start(music);
-		}
-	})
-	.catch((error) => {
-	console.log("error", error);
-	});
+const especificSong = buttonPlayer.addEventListener('click', (data) => {
+	musicController = data.songid;
+  const musicName = data.name;
+  music.src = data.audio;
+  music.load();
+  start(music);
+});
 
-
-function songCreation(audio) { /* Just works in modzilla */
-	let context = new AudioContext();   // is like an interface who has an  audio-processing graph built from the  audio 
-	let src = context.createMediaElementSource(audio); // is to create a new media with the audio 
-	let analyser = context.createAnalyser(); // to expose audio time and frequency data and create data visualisations.
-	analyser.connect(context.destination);  // lets you connect one of the node's outputs to a target // destination representing the final destination of all audio in the context example my speakers
-	analyser.fftSize = 512; // an unsigned long value and represents the window size in samples  (mathStuff https://en.wikipedia.org/wiki/Fast_Fourier_transform) El tamaño de FFT define el número de bins usados ​​para dividir la ventana en tiras iguales o bins. Por lo tanto, un contenedor es una muestra de espectro y define la resolución de frecuencia de la ventana.
-	src.connect(analyser);
-	let dataArray = new Uint8Array(analyser.frequencyBinCount); // typed array represents an array of 8-bit unsigned integers
-	let bufferLength = analyser.frequencyBinCount; // this generally equates to the number of data values you will have to play with for the visualization.
-	let barWidth = (width / bufferLength) * 1.5;
-	let barHeight;
-	let x = 0;
-
-	function songAnimation() {
-			x= 0;
-			analyser.getByteFrequencyData(dataArray)
-			ctx.fillStyle = "#2A2438";
-			ctx.fillRect(0, 0, width, height);
-			for (let i = 0; i < bufferLength; i++) {
-					barHeight = dataArray[i] + 150;
-					let r = barHeight / 1.8 + ((i/bufferLength)) - 15;
-					let g = 110 * (i/bufferLength);
-					let b = 120 + i ;
-					ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-					ctx.fillRect(x, height - barHeight, barWidth, barHeight);
-					x += barWidth + 1;
-			}
-			requestAnimationFrame(songAnimation);
-	}
-	songAnimation()
-};
+const SongsList = new SongPlayer(arrayOfSongs, musicController);
+SongsList.addPlaylist();
+SongsList.previous();
+SongsList.next();
--- js/modules/SongPlayer.js
+++ js/modules/SongPlayer.js
@@ -1,82 +1,44 @@
-export default class SongPlayer{
-    previousButton = document.querySelector('.previousButton')
-    nextButton = document.querySelector('.nextButton')
-    musicTitle = document.querySelector('.music__player--title span');
-    musicController = 0;
-    constructor(data){
-        this.data = data;
-    }
-    previous(){
-        this.previousButton.addEventListener('click', () =>{
-            this.musicController-=1;
-            if(this.musicController < 0){
-                this.musicController = this.data.length - 1;
-            }
-			this.musicTitle.innerHTML = this.data[this.musicController].name;
-        })
-    }
-    next(){
-        this.nextButton.addEventListener('click',() =>{
-            this.musicController+=1;
-            if(this.musicController > this.data.length - 1){
-                this.musicController = 0;
-            }
-			this.musicTitle.innerHTML = this.data[this.musicController].name;
-        })
-    }
-}
-
-
-
-
-
 /*
+export default class SongPlayer {
+  previousButton = document.querySelector('.previousButton');
+  nextButton = document.querySelector('.nextButton');
+  musicTitle = document.querySelector('.music__player--title span');
+  modal = document.querySelector('.music__player--modal');
+  constructor(arrayOfSongs, musicController) {
+    this.musicController = musicController; 
+    this.data = arrayOfSongs;
+  }
 
-function song(data){
-	music.src = data;
-	music.crossOrigin = "anonymous";
-	music.load();
-	PlaySong(music);
+  addPlaylist() {
+    this.addButton.addEventListener('click', () => {
+      if (!this.modal.classList.contains('visible')) {
+        this.modal.classList.add('visible');
+      } else {
+        this.modal.classList.remove('visible');
+      }
+    });
+  }
+
+  previous() {
+    this.previousButton.addEventListener('click', () => {
+      this.musicController -= 1;
+      if (this.musicController < 0) {
+        this.musicController = this.data.length - 1;
+      }
+      console.log(this.data[this.musicController].audio);
+      this.musicTitle.innerHTML = this.data[this.musicController].name;
+    });
+  }
+
+  next() {
+    this.nextButton.addEventListener('click', () => {
+      this.musicController += 1;
+      if (this.musicController > this.data.length - 1) {
+        this.musicController = 0;
+      }
+      console.log(this.data[this.musicController].audio);
+      this.musicTitle.innerHTML = this.data[this.musicController].name;
+    });
+  }
 }
-
-
-function previousSong(data){
-	previousButton.addEventListener('click', () =>{
-		playButton.src = '../img/stop-button.png';
-		musicController--;
-		if(musicController < 0){
-			musicController = data.length - 1;
-		}
-		console.log('next',data[musicController].audio)
-		song(data[musicController].audio);
-	})
-}
-
-
-function PlaySong(song){
-	playButton.addEventListener('click', (e) => {
-		songCreation(song)
-		if(e.target.classList.contains('active')){
-				playButton.src = '../img/play-button.png';
-				song.pause();
-		}else{
-				playButton.src = '../img/stop-button.png';
-				song.play();
-		}
-		e.target.classList.toggle('active');
-	})
-}
-
-function nextSong(data){
-	nextButton.addEventListener('click',() =>{
-		playButton.src = '../img/play-button.png';
-		musicController++;
-		if(musicController > data.length - 1){
-			musicController = 0;
-		}
-		console.log('next',data[musicController].audio)
-		song(data[musicController].audio);
-	})
-}
-
 */
