0c375f6805b2d411dcc916625f6c7b09d2810cd1
Karen Méndez <kmendezgz@gmail.com>
2021-07-26 09:39:44 -0600

    cleaning backend

--- routes/favourite.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routerFavourite = express.Router();
-const favouriteController = require('../controllers/favourite.controller');
-
-routerFavourite.post('/favourite', favouriteController.create);
-routerFavourite.get('/favourite', favouriteController.getFavourite);
-routerFavourite.put('/favourite', favouriteController.update);
-
-module.exports = routerFavourite;
--- routes/recent.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routerRecent = express.Router();
-const recentController = require('../controllers/recent.controller');
-
-routerRecent.post('/recent', recentController.create);
-routerRecent.get('/recent', recentController.getRecent);
-routerRecent.put('/recent', recentController.update);
-
-module.exports = routerRecent;
