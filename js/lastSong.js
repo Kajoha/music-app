454d7e868bc54a4f011b391076714d382b3ac6da
Ignacio Cl <juanigcl2532@gmail.com>
2021-08-02 11:35:01 -0600

    Modifico el js de lastSong

--- js/lastSong.js
+++ js/lastSong.js
@@ -1,12 +1,15 @@
-import { loadArtists } from "./home.js";
+import LoadArtists from './modules/artists.js';
 
 const LastSongButton = document.querySelector('.lastSong__player--button img');
-const apiArtists = 'https://kt2ul4cwza.execute-api.us-east-2.amazonaws.com/public/artists';
+
+fetch('https://kt2ul4cwza.execute-api.us-east-2.amazonaws.com/public/artists').then((response) => response.json()).then((data) => {
+    const load = new LoadArtists(data);
+    load.addArtists();
+});
+
 
 function lastSong(ActualSong){
     LastSongButton.dataset.audio = ActualSong.audio;
 }
 
-fetch(apiArtists).then((response) => response.json()).then((data) => {
-    loadArtists(data);
-});
+lastSong();
